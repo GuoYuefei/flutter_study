12a6870325250af2652d4795aadcb626319fc5ea
Solomon Guo <beconegood@gmail.com>
2018-12-31 22:33:09 +0800

    完成记录分数，比较数次后弹出喜好板，todo 喜好页面需要完善

--- favorite_pictures/lib/main.dart
+++ favorite_pictures/lib/main.dart
@@ -58,6 +58,8 @@ class PhotoDisplay extends StatefulWidget {
 }
 
 class PhotoDisplayState extends State<PhotoDisplay> {
+  static const int compare_num = 3;  
+  int _nowNum = 1;   
   final Icon iconState0 = new Icon(Icons.favorite_border);
   final Icon iconState1 = new Icon(
     Icons.favorite,
@@ -72,6 +74,23 @@ class PhotoDisplayState extends State<PhotoDisplay> {
 
   // TODO 用于记录此次用户选择喜爱照片的情况
   Map<int, int> favi = new Map<int, int>();
+
+  //记录的函数
+  //这个函数应该放入计时器的回调函数中调用
+  void _record() {
+    int id;
+    //如果icon1是喜欢状态那么就记录下图片1，否则
+    id = iconState1 == icon1 ? image1.id : image2.id;
+    //如果不是第一次喜欢该图片的话就计数加一
+    if(favi.containsKey(id)) {
+      favi[id]++;
+      return;
+    }
+    //第一次喜欢就将此id作为key放入map中
+    favi.putIfAbsent(id, ()=>1);
+  }
+
+
   PhotoDisplayState(int pnum) {
     
     this.pNum = pnum;
@@ -145,7 +164,7 @@ class PhotoDisplayState extends State<PhotoDisplay> {
     }
   }
 
-  //点击爱心之后的效果
+  //点击爱心1之后的效果
   void _pressFavi1() {
     //两者不能同时喜欢
     if (icon2 == this.iconState1) {
@@ -161,6 +180,7 @@ class PhotoDisplayState extends State<PhotoDisplay> {
     // _refresh();
   }
 
+  //点击爱心1之后的效果
   void _pressFavi2() {
     //两者不能同时喜欢
     if (icon1 == this.iconState1) {
@@ -200,14 +220,45 @@ class PhotoDisplayState extends State<PhotoDisplay> {
   //TODO 切换页面 这个不用路由实现，在选择完喜欢的照片后不能再退回之前的状态了。
   //选择完喜欢照片后两秒内自动跳转，若此时取消喜欢就取消定时跳转
   void _refresh() {
+    //刷新之前需要记录用户喜欢哪张图片
+    _record();
+
     //主要是两张图片的刷新
     setState(() {
-      image1 = _randomPicture(pNum, exceptions: [image1, image2]);
+      if(_nowNum == compare_num) {
+        //如果没达到设置的比较上限
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context) => Preference()), (Route r) => true);
+      } else {
+        _nowNum++;   //比较次数加一
+        image1 = _randomPicture(pNum, exceptions: [image1, image2]);
 
-      image2 = _randomPicture(pNum, exceptions: [image1, image2]);
+        image2 = _randomPicture(pNum, exceptions: [image1, image2]);
 
-      icon1 = this.iconState0;
-      icon2 = this.iconState0;
+        icon1 = this.iconState0;
+        icon2 = this.iconState0;
+      }
     });
   }
+}
+
+
+//显示页面需要一个不一样的wigets
+class Preference extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return PreferenceState();
+  }
+
+}
+
+class PreferenceState extends State<Preference> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Text("1111111111");
+  }
+
+
+
 }
